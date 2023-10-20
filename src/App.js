7c8f952917cdacc18f005ee7031268e275bde205
aman8988 <amantrivedi8988@gmail.com>
2023-10-20 14:07:54 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import "./App.css";
 import Calendar from "./Calendar";
 
--- src/Calendar.jsx
+++ src/Calendar.jsx
@@ -7,12 +7,163 @@ import moment from "moment";
 import momentTZ from "moment-timezone";
 momentTZ.tz.setDefault("UTC");
 
+const data = [
+  {
+    name: "Karen Garcia",
+    date: "2023-10-20",
+    time: "10:00 AM",
+  },
+  {
+    name: "Joseph Taylor",
+    date: "2023-10-20",
+    time: "10:30 AM",
+  },
+  {
+    name: "Daniel Hall",
+    date: "2023-10-20",
+    time: "10:30 PM",
+  },
+  {
+    name: "Matthew Young",
+    date: "2023-10-20",
+    time: "5:30 PM",
+  },
+  {
+    name: "James Wilson",
+    date: "2023-10-21",
+    time: "9:00 PM",
+  },
+  {
+    name: "Matthew Young",
+    date: "2023-10-21",
+    time: "12:30 PM",
+  },
+  {
+    name: "Robert Lee",
+    date: "2023-10-21",
+    time: "9:00 AM",
+  },
+  {
+    name: "Daniel Hall",
+    date: "2023-10-21",
+    time: "10:30 AM",
+  },
+  {
+    name: "Linda Scott",
+    date: "2023-10-21",
+    time: "6:30 PM",
+  },
+  {
+    name: "Patricia Lewis",
+    date: "2023-10-29",
+    time: "9:30 AM",
+  },
+  {
+    name: "Nancy Hernandez",
+    date: "2023-10-29",
+    time: "9:00 AM",
+  },
+  {
+    name: "William Anderson",
+    date: "2023-10-29",
+    time: "10:00 PM",
+  },
+  {
+    name: "Jennifer Martinez",
+    date: "2023-10-30",
+    time: "11:30 AM",
+  },
+  {
+    name: "Karen Garcia",
+    date: "2023-10-31",
+    time: "10:00 AM",
+  },
+  {
+    name: "Jane Smith",
+    date: "2023-10-30",
+    time: "2:00 PM",
+  },
+  {
+    name: "Mary Wilson",
+    date: "2023-10-30",
+    time: "10:30 AM",
+  },
+  {
+    name: "Linda Scott",
+    date: "2023-11-01",
+    time: "10:30 AM",
+  },
+  {
+    name: "Nancy Hernandez",
+    date: "2023-11-01",
+    time: "11:30 AM",
+  },
+  {
+    name: "Patricia Lewis",
+    date: "2023-11-02",
+    time: "11:30 AM",
+  },
+  {
+    name: "Jane Smith",
+    date: "2023-11-02",
+    time: "10:00 AM",
+  },
+  {
+    name: "Mary Wilson",
+    date: "2023-11-02",
+    time: "1:30 PM",
+  },
+  {
+    name: "Karen Garcia",
+    date: "2023-11-02",
+    time: "2:00 PM",
+  },
+  {
+    name: "Michael Davis",
+    date: "2023-11-03",
+    time: "10:00 AM",
+  },
+  {
+    name: "Jane Smith",
+    date: "2023-11-03",
+    time: "3:00 PM",
+  },
+  {
+    name: "Lisa Brown",
+    date: "2023-11-04",
+    time: "10:00 AM",
+  },
+  {
+    name: "Patricia Lewis",
+    date: "2023-11-04",
+    time: "11:30 AM",
+  },
+  {
+    name: "Donna Clark",
+    date: "2023-11-04",
+    time: "6:00 PM",
+  },
+  {
+    name: "Nancy Hernandez",
+    date: "2023-11-04",
+    time: "5:00 PM",
+  },
+  {
+    name: "Mary Wilson",
+    date: "2023-11-03",
+    time: "9:00 AM",
+  },
+  {
+    name: "David Johnson",
+    date: "2023-11-01",
+    time: "4:30 PM",
+  },
+];
+
 function Calendar() {
   const [currentWeek, setCurrentWeek] = useState(moment());
   const [selectedTimezone, setSelectedTimezone] = useState("UTC-0");
-  const [events, setEvents] = useState(
-    JSON.parse(localStorage.getItem("events")) || []
-  );
+  const [events, setEvents] = useState(data);
   const [datechange, setDatechange] = useState(currentWeek.date());
 
   const handlePreviousWeek = () => {
